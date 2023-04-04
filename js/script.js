5522c15d33968b10e66723af3e4bdc59a1b2b900
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-04-04 12:39:27 -0400

    JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares.abdelkader
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const aBase = parseInt(document.getElementById('aBase').value)
+  const bBase = parseInt(document.getElementById('bBase').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  // process 
+  const area = (aBase + bBase) * (1 / 2) * height
+
+  // output
+  document.getElementById('area').innerHTML = '<p>Area is: ' + area.toString() + ' mm²</p>'
+}
